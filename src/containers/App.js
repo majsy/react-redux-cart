5e2f2f46de0c19f3c8316b82102eb72e742b0bef
majsy <madeleineauvinen@gmail.com>
2019-08-28 20:05:59 -0400

    install node-sass set up basic scss structure add breakpoints add media query mixins

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
+import '../scss/components/_App.scss'
 
 const App = () => (
-  <div>
+  <div className="container">
     <h2>Shopping Cart Example</h2>
     <hr/>
     <ProductsContainer />
