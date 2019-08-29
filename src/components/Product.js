bc5876e7244973b54dd861757024b423e528a134
majsy <madeleineauvinen@gmail.com>
2019-08-28 23:33:34 -0400

    update product image layout add button styles fix product detail layout

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import '../scss/components/_product.scss'
 
 const Product = ({ price, inventory, title }) => (
   <div className="product">
-    {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
+    <div className="title-container">
+      <h2 className="title">{title}</h2>
+      <h3 className="price">&#36;{price}</h3>
+    </div>
+
+    <div className="quantity-container">
+      {inventory ? <p>{inventory} remaining</p> : null}
+    </div>
   </div>
 )
 
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -5,17 +5,25 @@ import '../scss/components/_product-item.scss'
 
 const ProductItem = ({ product, onAddToCartClicked }) => (
   <div className="product-item">
-    <div className="image-container"></div>
-    
-    <Product
-      title={product.title}
-      price={product.price}
-      inventory={product.inventory} />
-    <button
-      onClick={onAddToCartClicked}
-      disabled={product.inventory > 0 ? '' : 'disabled'}>
-      {product.inventory > 0 ? 'Add to cart' : 'Sold Out'}
-    </button>
+    <div className="image-container">
+      <div className="aspect-ratio-container">
+        <img className="image" src="https://via.placeholder.com/400" />
+      </div>
+    </div>
+
+    <div className="details-container">
+      <Product
+        title={product.title}
+        price={product.price}
+        inventory={product.inventory} />
+
+      <button className="button"
+        onClick={onAddToCartClicked}
+        disabled={product.inventory > 0 ? '' : 'disabled'}>
+        {product.inventory > 0 ? 'Add to cart' : 'Sold Out'}
+      </button>
+    </div>
+  
   </div>
 )
 
